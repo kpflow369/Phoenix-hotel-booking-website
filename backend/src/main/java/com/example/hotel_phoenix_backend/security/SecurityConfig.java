54241a462fea80329ef58e8b564e41531e41b874
Khushi Patil <khushipatil187@gmail.com>
2026-06-17 12:36:24 +0530

    Update SecurityConfig.java

--- backend/src/main/java/com/example/hotel_phoenix_backend/security/SecurityConfig.java
+++ backend/src/main/java/com/example/hotel_phoenix_backend/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.hotel_phoenix_backend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -13,11 +14,13 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         http
-            .csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll()
-            );
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
 
         return http.build();
     }
