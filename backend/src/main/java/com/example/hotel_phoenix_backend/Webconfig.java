1f9e7cd9d66c55788c910242acc2bf276b9546a8
Khushi Patil <khushipatil187@gmail.com>
2026-06-17 12:02:04 +0530

    Update Webconfig.java

--- backend/src/main/java/com/example/hotel_phoenix_backend/Webconfig.java
+++ backend/src/main/java/com/example/hotel_phoenix_backend/Webconfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
-public class WebConfig {
+public class Webconfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
